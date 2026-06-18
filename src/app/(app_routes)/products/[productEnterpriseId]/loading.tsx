a2d02953d63a02ff632a7827270266225eb1166d
LeomirDias <leomirdiasti@outlook.com>
2026-06-18 17:21:00 -0300

    refactor<app routes>: Remover componentes obsoletos relacionados à visualização e filtragem de produtos, simplificando a estrutura do código e melhorando a manutenção da aplicação.

--- src/app/(app_routes)/products/[productEnterpriseId]/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { ProductDetailContentLoading } from "@/app/(app_routes)/products/_components/products-route-loading"
-
-export default function ProductEnterpriseLoading() {
-  return (
-    <main className="mx-auto flex w-full flex-col gap-6 p-4 md:p-8">
-      <ProductDetailContentLoading />
-    </main>
-  )
-}
--- src/app/(app_routes)/products/promotional-prices/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import { PromotionalPricesListPage } from "@/app/(app_routes)/products/_components/promotional-prices-list-page"
-import { PROMOTIONAL_PRICES_ROUTE_CONFIG } from "@/modules/products/products-route-config"
-
-export default function PromotionalPricesPage() {
-  return <PromotionalPricesListPage config={PROMOTIONAL_PRICES_ROUTE_CONFIG} />
-}
